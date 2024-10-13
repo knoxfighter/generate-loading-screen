ac25ea5d7c6324da734250c2f3fa752a24faf72c
darthmaim <git@darthmaim.de>
2024-10-13 11:27:05 +0200

    Use zod to validate addon configs

---
+++ src/schema.ts
@@ -0,0 +1,68 @@
+import { z } from 'zod'
+
+// TODO: can this be removed?
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const downloadType = z.enum(['archive', 'dll'])
+
+const installMode = z.enum(['gw2load', 'arc'])
+
+const version = z.tuple([z.number(), z.number(), z.number(), z.number()])
+export type Version = z.infer<typeof version>
+
+const release = z.object({
+  id: z.string(),
+  name: z.string(),
+  version,
+  version_str: z.string(),
+  download_url: z.string(),
+  asset_index: z.number().optional()
+})
+export type Release = z.infer<typeof release>
+
+const pkg = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  tooltip: z.string(),
+  website: z.string(),
+  developer: z.string(),
+  issue_tracker: z.string().optional(),
+  vcs: z.string().optional(),
+
+  dependencies: z.array(z.string()),
+  optional_dependencies: z.array(z.string()),
+  conflicts: z.array(z.string())
+})
+
+const githubHost = z.object({
+  url: z.string()
+})
+export type GithubHost = z.infer<typeof githubHost>
+
+const standaloneHost = z.object({
+  url: z.string(),
+  version_url: z.string(),
+  prerelease_url: z.string().optional(),
+  // TODO: should this be optional?
+  prerelease_version_url: z.string()
+})
+export type StandaloneHost = z.infer<typeof standaloneHost>
+
+const host = z.union([
+  z.object({ github: githubHost }),
+  z.object({ standalone: standaloneHost })
+])
+
+const installation = z.object({
+  mode: installMode
+})
+
+export const plugin = z.object({
+  package: pkg,
+  host,
+  installation,
+  release: release.optional(),
+  prerelease: release.optional(),
+  addon_names: z.array(z.string()).optional()
+})
+export type Plugin = z.infer<typeof plugin>
